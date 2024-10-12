8f3552299354f1f337b7e35486d2138fb8d4a1b8
Myslitel666 <tretyakov_02@bk.ru>
2024-10-12 12:37:27 +0500

    The width setting is performed automatically.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,46 +17,50 @@
   let m = 2;
   let mb = `mb-${m}`;
   let mr = `mr-${m}`;
+  let wLabel = `w-52`;
+  let wInput = `w-40`;
 </script>
 
 <div class={`p-${m}`}>
   <div class="flex items-center {mb}">
-    <p class="w-52 text-right {mr}">Deposit amount:</p>
+    <p class="{wLabel} text-right {mr}">Deposit amount:</p>
     <!-- Установим фиксированную ширину для метки -->
     <input
       on:input={handleInput}
       type="text"
       placeholder="100 000"
-      class="input input-bordered w-40 {mr}"
+      class="input input-bordered {wInput} {mr}"
     />
     <p>₽</p>
   </div>
   <div class="flex items-center {mb}">
-    <p class="w-52 text-right {mr}">Income:</p>
+    <p class="{wLabel} text-right {mr}">Income:</p>
     <!-- Установим фиксированную ширину для метки -->
     <input
       on:input={handleInput}
       type="text"
       placeholder="20"
-      class="input input-bordered w-40 {mr}"
+      class="input input-bordered {wInput} {mr}"
     />
     <p>%</p>
   </div>
   <div class="flex items-center {mb}">
-    <p class="w-52 text-right {mr}">Duration of the investment:</p>
+    <p class="{wLabel} text-right {mr}">Duration of the investment:</p>
     <!-- Установим фиксированную ширину для метки -->
     <input
       on:input={handleInput}
       type="text"
       placeholder="1"
-      class="input input-bordered w-40 {mr}"
+      class="input input-bordered {wInput} {mr}"
     />
 
     <details class="dropdown">
-      <summary class="btn" on:click={toggleDropdown}>open or close</summary>
+      <summary class="btn {wInput}" on:click={toggleDropdown}>
+        open or close
+      </summary>
       {#if isOpen}
         <ul
-          class="menu dropdown-content bg-base-100 rounded-box z-[1] w-52 p-2 shadow"
+          class="menu dropdown-content bg-base-100 rounded-box z-[1] {wInput} p-2 shadow"
         >
           <li><a on:click={closeDropdown}>Years</a></li>
           <li><a on:click={closeDropdown}>Months</a></li>
