f4b36577c8c8d6797a1001aaf6749a6a944e6caf
Myslitel666 <tretyakov_02@bk.ru>
2024-10-12 12:31:18 +0500

    Indentation is automated.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,41 +1,70 @@
 <script>
+  let isOpen = false;
+
+  function toggleDropdown() {
+    isOpen = true;
+  }
+
+  function closeDropdown() {
+    isOpen = false;
+  }
+
   function handleInput(event) {
     // Удаляем все символы, кроме цифр
     event.target.value = event.target.value.replace(/\D/g, "");
   }
+
+  let m = 2;
+  let mb = `mb-${m}`;
+  let mr = `mr-${m}`;
 </script>
 
-<div class="p-8">
-  <div class="flex items-center mb-2">
-    <p class="w-32 text-right mr-2">Deposit amount:</p>
+<div class={`p-${m}`}>
+  <div class="flex items-center {mb}">
+    <p class="w-52 text-right {mr}">Deposit amount:</p>
     <!-- Установим фиксированную ширину для метки -->
     <input
       on:input={handleInput}
       type="text"
       placeholder="100 000"
-      class="input input-bordered w-40"
+      class="input input-bordered w-40 {mr}"
     />
-    <p class="ml-2">₽</p>
+    <p>₽</p>
   </div>
-  <div class="flex items-center">
-    <p class="w-32 text-right mr-2">Income:</p>
+  <div class="flex items-center {mb}">
+    <p class="w-52 text-right {mr}">Income:</p>
     <!-- Установим фиксированную ширину для метки -->
     <input
       on:input={handleInput}
       type="text"
       placeholder="20"
-      class="input input-bordered w-40"
+      class="input input-bordered w-40 {mr}"
     />
-    <p class="ml-2">%</p>
+    <p>%</p>
+  </div>
+  <div class="flex items-center {mb}">
+    <p class="w-52 text-right {mr}">Duration of the investment:</p>
+    <!-- Установим фиксированную ширину для метки -->
+    <input
+      on:input={handleInput}
+      type="text"
+      placeholder="1"
+      class="input input-bordered w-40 {mr}"
+    />
+
+    <details class="dropdown">
+      <summary class="btn" on:click={toggleDropdown}>open or close</summary>
+      {#if isOpen}
+        <ul
+          class="menu dropdown-content bg-base-100 rounded-box z-[1] w-52 p-2 shadow"
+        >
+          <li><a on:click={closeDropdown}>Years</a></li>
+          <li><a on:click={closeDropdown}>Months</a></li>
+        </ul>
+      {/if}
+    </details>
+
+    <style>
+    </style>
   </div>
 </div>
-
-<details class="dropdown">
-  <summary class="btn m-1">open or close</summary>
-  <ul
-    class="menu dropdown-content bg-base-100 rounded-box z-[1] w-52 p-2 shadow"
-  >
-    <li><a>Item 1</a></li>
-    <li><a>Item 2</a></li>
-  </ul>
-</details>
