66453da9f3a1df132090ef458c560213535cb375
Myslitel666 <tretyakov_02@bk.ru>
2024-10-19 18:01:19 +0500

    n

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,16 @@
-<p>Hello</p>
+<script>
+	import { ThemeProvider } from "svelte-elegant";
+</script>
+
+<ThemeProvider>
+	<div class = 'header'>
+
+	</div>
+</ThemeProvider>
+
+<style>
+	.header {
+		height: 2rem;
+		color: #e8e8e8;
+	}
+</style>
