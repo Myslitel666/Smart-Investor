ed548fb2e2fe3231713645f471ed790e2001c090
Myslitel666 <tretyakov_02@bk.ru>
2024-10-20 03:02:16 +0500

    Updates

--- _app/immutable/nodes/0.Bz8O5pGy.js
+++
@@ -1,1 +0,0 @@
-import{a as p,t as e}from"../chunks/disclose-version.DvcYe3sd.js";import{c as r,r as t}from"../chunks/runtime.Bgr7HT5E.js";import{s as n}from"../chunks/slot.DQC4Sb1T.js";var d=e('<div class="app"><main><!></main></div>');function f(i,m){var a=d(),o=r(a),s=r(o);n(s,m,"default",{}),t(o),t(a),p(i,a)}export{f as component};
