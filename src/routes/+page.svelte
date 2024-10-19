d83e152c15148aeb8114ddc1a1b6e2e960817b7b
Myslitel666 <tretyakov_02@bk.ru>
2024-10-19 18:49:54 +0500

    n

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,13 +4,43 @@
 
 <ThemeProvider>
 	<div class = 'header'>
-
+		<img src = './USD-Coin-Logo-PNG-Images.png' class = 'logo'>
+	</div>
+	<div class = 'content'>
+		<img src = './smart-investor-logo.png' class = 'box-logo'>
 	</div>
 </ThemeProvider>
 
 <style>
 	.header {
-		height: 3.66rem;
+		height: 4rem;
 		background-color: #f9f9f9;
+		display: flex;
+		align-items: center; /* Центрирует по вертикали */
+	}
+
+	.content {
+		display: flex;
+		justify-content: center; /* Центрирует по горизонтали */
+		height: 100vh;
+	}
+
+	.logo {
+		margin-left: 0.5rem;
+		width: 4.25rem;
+		height: 4.25rem;
+		transition: transform 0.5s;
+	}
+
+	.box-logo {
+		margin-top: 1rem;
+		width: 4.25rem;
+		height: 4.25rem;
+		border-radius: 50%;
+	}
+
+	.logo:hover {
+		cursor: pointer;
+		transform: scale(1.2);
 	}
 </style>
